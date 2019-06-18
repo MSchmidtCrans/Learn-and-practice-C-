2a09f5835208323a0509714cf1adf78f20e6bca0
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-18 11:03:05 +0200

    Numbers in C#

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,9 +4,75 @@ namespace MyApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
-            Console.WriteLine("Hello World!");
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence() 
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+        }
+
+        static void WorkWithDoubles ()
+        {
+             //WorkingWithIntegers();
+             //OrderPrecedence();
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a  + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void WorkingWithDecimals()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+
+        static void Main (string[] args)
+        {
+            double radius = 2.50;
+            double area = radius * radius * Math.PI;
+            Console.WriteLine($"The area for radius { radius } is { area }");
         }
     }
 }
